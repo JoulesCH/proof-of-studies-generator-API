4704f5e1815e253b255b186e4d23e6b803f54350
Julio César Hernández <joules.hdz@gmail.com>
2023-02-04 12:07:25 -0600

    :sparkles: Login implemented

---
+++ utils/students_data/headers.py
@@ -0,0 +1,25 @@
+headers_login = {
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Language': 'es-419,es;q=0.9',
+    'Cache-Control': 'no-cache',
+    'Connection': 'keep-alive',
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Host': 'sistemasdp.sip.ipn.mx',
+    'Origin': 'https://sistemasdp.sip.ipn.mx',
+    'Referer': 'https://sistemasdp.sip.ipn.mx/WebTrayectoria/MAtricula/Login_mat_a.aspx',
+    'sec-ch-ua': '"Chromium";v="90", "Opera";v="76", ";Not A Brand";v="99"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+    'Sec-Fetch-Dest': 'document',
+    'Sec-Fetch-Mode': 'navigate',
+    'Sec-Fetch-Site': 'same-origin',
+    'Sec-Fetch-User': '?1',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36 OPR/89.0.4447.48'
+}
+form_data_login = {
+    '__EVENTTARGET':'',
+    '__EVENTARGUMENT':'',
+    'ctl00$MainContent$btnValidar': 'Iniciar sesión'
+}
---
+++ utils/students_data/login.py
@@ -0,0 +1,48 @@
+import requests
+from bs4 import BeautifulSoup
+
+from .headers import headers_login, form_data_login
+
+def login(user, password):
+    """
+    Se conecta al sitio para iniciar sesión
+    """
+
+    url_login = 'https://sistemasdp.sip.ipn.mx/WebTrayectoria/MAtricula/Login_mat_a.aspx'
+
+    session = requests.Session()
+
+    # Login para obtener los hidden inputs
+    response_login_get = session.get(url_login)
+    get_soup = BeautifulSoup(response_login_get.text, "html.parser")
+    inputs = get_soup.find_all('input')
+    VIEWSTATE, VIEWSTATEGENERATOR, EVENTVALIDATION= inputs[:3]
+
+    # Se generan los headers y el formulario
+    form_data_login['__VIEWSTATE'] = VIEWSTATE.get('value')
+    form_data_login['__VIEWSTATEGENERATOR'] = VIEWSTATEGENERATOR.get('value')
+    form_data_login['__EVENTVALIDATION'] = EVENTVALIDATION.get('value')
+    form_data_login['ctl00$MainContent$txtUsuario'] = user
+    form_data_login['ctl00$MainContent$txtPwd'] = password
+
+
+    # Hacemos login y obtenemos la cookie
+    response_login = session.post(
+        url_login,
+        headers=headers_login,
+        data=form_data_login
+    )
+
+    cookie_ASPNETSessionId = session.cookies.get('ASP.NET_SessionId')
+    
+    soup_login_post = BeautifulSoup(response_login.text, "html.parser")
+    inputs = soup_login_post.find_all('input')
+    
+    return dict(
+        session=session,
+        cookie=cookie_ASPNETSessionId,
+        VIEWSTATE=inputs[2],
+        VIEWSTATEGENERATOR=inputs[3], 
+        EVENTVALIDATION=inputs[4],
+        cookies=session.cookies
+    )
